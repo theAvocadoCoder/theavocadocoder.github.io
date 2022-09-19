a7cebc21fb98e2450703aa0217eadc83929bd644
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-09-19 16:23:56 +0100

    Add a highlight color when tab is selected

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,75 @@
 
+/* Functions */
+
+function addHighlightColor(activeTab) {
+  navLinks.forEach(link => {
+    if (link !== activeTab) {
+      link.classList.toggle("text-green-600", false);
+    } else {
+      link.classList.toggle("text-green-600", true);
+    }
+  });
+
+}
+
+
+/* Links */
+
 const homeNavLink = document.querySelector("#home-nav-link"),
   aboutNavLink = document.querySelector("#about-nav-link"),
   contactNavLink = document.querySelector("#contact-nav-link"),
   projectsNavLink = document.querySelector("#projects-nav-link"),
   goHomeLink = document.querySelector("#go-home");
 
+const navLinks = [homeNavLink, projectsNavLink, aboutNavLink, contactNavLink];
+
+
+/* Tabs & Sections */
+
+const homeTab = document.querySelector("#home"),
+  inProgressTab = document.querySelector("#in-progress"),
+  aboutSection = document.querySelector("#about"),
+  contactSection = document.querySelector("#contact");
+
+
+/* Event Listeners */
+
 goHomeLink.addEventListener("click", () => {
-  document.querySelector("#home").classList.toggle("hidden", false);
-  document.querySelector("#in-progress").classList.toggle("hidden", true);
+  homeTab.classList.toggle("hidden", false);
+  inProgressTab.classList.toggle("hidden", true);
+  addHighlightColor(homeNavLink);
 });
+
 homeNavLink.addEventListener("click", () => {
-  document.querySelector("#home").classList.toggle("hidden", false);
-  document.querySelector("#in-progress").classList.toggle("hidden", true);
+  homeTab.classList.toggle("hidden", false);
+  inProgressTab.classList.toggle("hidden", true);
+  addHighlightColor(homeNavLink);
 });
+
 aboutNavLink.addEventListener("click", () => {
-  document.querySelector("#home").classList.toggle("hidden", false);
-  document.querySelector("#in-progress").classList.toggle("hidden", true);
+  homeTab.classList.toggle("hidden", false);
+  inProgressTab.classList.toggle("hidden", true);
+  addHighlightColor(aboutNavLink);
 });
+
 contactNavLink.addEventListener("click", () => {
-  document.querySelector("#home").classList.toggle("hidden", false);
-  document.querySelector("#in-progress").classList.toggle("hidden", true);
+  homeTab.classList.toggle("hidden", false);
+  inProgressTab.classList.toggle("hidden", true);
+  addHighlightColor(contactNavLink);
 });
+
 projectsNavLink.addEventListener("click", () => {
-  document.querySelector("#home").classList.toggle("hidden", true);
-  document.querySelector("#in-progress").classList.toggle("hidden", false);
+  homeTab.classList.toggle("hidden", true);
+  inProgressTab.classList.toggle("hidden", false);
+  addHighlightColor(projectsNavLink);
 });
 
 // function switchTab() {
 //   e.preventDefault();
   
-//   const homeTab = document.querySelector("#home"),
+//   const homeTab = homeTab,
 //   projectTab = document.querySelector("#project"),
-//   inProgressTab = document.querySelector("#in-progress");
+//   inProgressTab = inProgressTab;
 
 //   if (tab === "home") {
 //     homeTab.classList.toggle("hidden");
